296c50582dc0aeaca1319dce28a6fa778446a160
nahomsof <nahomsileshi21@gmail.com>
2026-09-23 17:15:36 +0300

    feat: track text input

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -28,10 +26,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _typedCharacters = 0;
+  final TextEditingController typingController = TextEditingController();
 
-  void _incrementCounter() {
+  void typedCharacters() {
     setState(() {
-      _typedCharacters++;
+      _typedCharacters = typingController.text.length;
     });
   }
 
@@ -40,28 +39,24 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('You have written this amount of words'),
             Text(
-
-              '$_typedCharacters',
+              'Characters: $_typedCharacters',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            
+            TextField(
+              controller: typingController,
+              onChanged: (value) => typedCharacters(),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
